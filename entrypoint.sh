ad79d73fb9f8827603ce4de5a93c71ceb3399a53
aruberutsu <aruberutsu@users.noreply.github.com>
2022-03-23 00:05:19 +0100

    Add autoupdate for GeoIP databases

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+if [[ $UPDATE_GEOIP == "yes" ]]; then
+    echo "*** Trying to update GeoIP files, please wait..."
+    if wget https://mailfud.org/geoip-legacy/GeoIPCity.dat.gz -O /tmp/GeoIPCity.dat.gz ; then
+        zcat /tmp/GeoIPCity.dat.gz > /usr/local/etc/ntop/GeoLiteCity.dat
+        rm /tmp/GeoIPCity.dat.gz
+    fi
+    if wget https://mailfud.org/geoip-legacy/GeoIPASNum.dat.gz -O /tmp/GeoIPASNum.dat.gz ; then
+        zcat /tmp/GeoIPASNum.dat.gz > /usr/local/etc/ntop/GeoIPASNum.dat
+        rm /tmp/GeoIPASNum.dat.gz
+    fi
+fi
+
 /usr/local/bin/ntop --set-admin-password="$NTOP_PASSWD"
 /usr/local/bin/ntop "$NTOP_ARGS"
